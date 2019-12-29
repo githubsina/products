cdf32e8ab649feb2cf40f0c66871f5d66fabfc40
Caroline <shanqinglan1@163.com>
2019-12-29 15:30:55 +0800

    add procducts.py

--- products.py
+++ products.py
@@ -19,11 +19,16 @@ print(products)
 
 
 '''
-github 上传指令 
+github 后续上传更新指令 
 git add products.py
 git commit -m " add procducts.py
 git push origin master
 '''
 
-
-	
+'''github 初次上传创建文件指令
+echo "# products" >> README.md
+git init
+git commit -m "first commit"
+git remote add origin https://github.com/githubsina/products.git	
+git push -u origin master
+'''
